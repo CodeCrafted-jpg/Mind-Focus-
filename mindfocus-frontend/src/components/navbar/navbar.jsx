5dd24a44bf820c9fcb8be5d37b12ee1fd8fa49cb
Sayan Mallick <malsayan6@gmail.com>
2025-06-18 09:43:35 +0530

    Final commit

--- mindfocus-frontend/src/components/navbar/navbar.jsx
+++ mindfocus-frontend/src/components/navbar/navbar.jsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   const [user, setUser] = useState(null);
   const [showLoginPopup, setShowLoginPopup] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     const loggedInUser = authService.getUser();
@@ -23,34 +24,52 @@ const Navbar = () => {
   const handleLogout = () => {
     authService.logout();
     setUser(null);
-    // window.location.reload()
+  };
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMobileMenuOpen(false);
   };
 
   return (
     <>
       <nav className="navbar">
-        <Link to={'/'} className="navbar-brand">
-        <div >
-          <Logo />
-         Mind-Focus+ 
+        <Link to="/" className="navbar-brand" onClick={closeMenu}>
+          <div>
+            <Logo />
+            Mind-Focus+
+          </div>
+        </Link>
+
+        <div className="burger" onClick={toggleMobileMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
         </div>
-</Link>
-        <ul className="navbar-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#features">Features</a></li>
-          <li><a href="#">Join</a></li>
-          {user && (
+
+        <ul className={`navbar-links ${isMobileMenuOpen ? 'mobile-open' : ''}`}>
+           
+
+          <li><a href="/" onClick={closeMenu}>Home</a></li>
+          <li><a href="#" onClick={closeMenu}>About</a></li>
+          <li><a href="#features" onClick={closeMenu}>Features</a></li>
+          <li><a href="#" onClick={closeMenu}>Join</a></li>
+         {user  &&(
             <>
-              <li><Link to={"/focus"}>Focus</Link></li>
-              <li><Link to={"/groups"}>Groups</Link></li>
+              <li><Link to="/focus" onClick={closeMenu}>Focus</Link></li>
+              <li><Link to="/groups" onClick={closeMenu}>Groups</Link></li>
+             
             </>
           )}
         </ul>
+
         <div className="navbar-auth">
           {user ? (
             <>
-            <Link to={'/profile'} > <button className="profile-button">👤 {user.username}</button></Link>
+              <Link to="/profile"><button className="profile-button">👤 {user.username}</button></Link>
               <button className="logout-button" onClick={handleLogout}>Logout</button>
             </>
           ) : (
